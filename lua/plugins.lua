c155e72d0bfedcf82c15ac274134e50ec7a5b5c6
narcissus <1044532074@qq.com>
2022-12-14 11:01:06 +0800

    fix telescope current_buffer_fuzzy_find bug

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -54,7 +54,7 @@ packer.startup({
     -- telescope
     use({
       'nvim-telescope/telescope.nvim',
-      tag = '0.1.0',
+      branch = '0.1.x',
       requires = {
         'nvim-lua/plenary.nvim',
       },
