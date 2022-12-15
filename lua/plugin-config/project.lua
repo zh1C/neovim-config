1399f48660c67433afd2ef69620cb06d9ccd1f50
narcissus <1044532074@qq.com>
2022-12-15 21:12:09 +0800

    fix project.lua config bug

--- lua/plugin-config/project.lua
+++ lua/plugin-config/project.lua
@@ -9,11 +9,11 @@ vim.g.nvim_tree_respect_buf_cwd = 1
 
 project.setup({
   detection_methods = { 'pattern' },
-  patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', '.sln' },
+  patterns = { '.git', '.project', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', '.sln' },
 })
 
-local status, telescope = pcall(require, 'telescope')
-if not status then
+local statu, telescope = pcall(require, 'telescope')
+if not statu then
   vim.notify('没有找到 telescope')
   return
 end
