6d904be797aa22ecb25f6e3acd8a9be2906d1474
narcissus <1044532074@qq.com>
2023-06-13 18:33:19 +0800

    [feature]packer use bootstrapping.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,18 @@
 local packer = require('packer')
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 packer.startup({
   function(use)
     -- Packer 可以管理自己本身
@@ -217,6 +231,11 @@ packer.startup({
     use({ 'jbyuki/one-small-step-for-vimkind' })
 
     ------------------------ End plugin-----------------------------
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      require('packer').sync()
+    end
   end,
   config = {
     display = {
