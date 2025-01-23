b87fccaedef8d5ce426d3f09226a31535da48f2a
dengzhicheng03 <dengzhicheng03@meituan.com>
2025-01-23 11:26:56 +0800

    feat: 插件更新, 禁用gap, gitsigns配置更新

--- lua/plugin-config/gitsigns.lua
+++ lua/plugin-config/gitsigns.lua
@@ -8,11 +8,11 @@ end
 gitsigns.setup({
   -- 字母图标 A 增加，C修改，D 删除
   signs = {
-    add = { hl = 'GitSignsAdd', text = 'A|', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-    change = { hl = 'GitSignsChange', text = 'C|', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    delete = { hl = 'GitSignsDelete', text = 'D_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    topdelete = { hl = 'GitSignsDelete', text = 'D‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = 'D~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    add = { text = 'A|' },
+    change = { text = 'C|' },
+    delete = { text = 'D_' },
+    topdelete = { text = 'D‾' },
+    changedelete = { text = 'D~' },
   },
   current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
